43ee7e28e9b331d78c786ce150405b24b822092b
wendymackennah <wendymackennah5@gmail.com>
2024-02-03 21:58:53 +0300

    completed assignment

--- src/Parent.js
+++ src/Parent.js
@@ -1,15 +1,24 @@
+// Parent.js
+
 import React, { useState } from "react";
-import { getRandomColor } from "./randomColorGenerator.js";
+import { getRandomColor } from "./randomColorGenerator";
 import Child from "./Child";
 
 function Parent() {
   const randomColor = getRandomColor();
   const [color, setColor] = useState(randomColor);
+  const [childrenColor, setChildrenColor] = useState("#FFF");
+
+  function handleChangeColor(newChildColor) {
+    const newRandomColor = getRandomColor();
+    setColor(newRandomColor);
+    setChildrenColor(newChildColor);
+  }
 
   return (
     <div className="parent" style={{ backgroundColor: color }}>
-      <Child />
-      <Child />
+      <Child color={childrenColor} onChangeColor={handleChangeColor} />
+      <Child color={childrenColor} onChangeColor={handleChangeColor} />
     </div>
   );
 }
